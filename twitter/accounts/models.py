3dc335efdc9e61669f74fa024539380910cf8b46
RichardMatheus929 <richard.ataliba.711@ufrn.edu.br>
2025-04-27 20:45:14 -0300

    feat: update user model reference for authentication provide

--- twitter/accounts/models.py
+++ twitter/accounts/models.py
@@ -2,12 +2,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser,Permission
 
-# Create your models here.
 class User(AbstractBaseUser):
+
     name = models.CharField(max_length=100)
     username = models.CharField(max_length=100, unique=True)
     email = models.EmailField(unique=True)
     create_account = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'username'
 
     def __str__(self) -> str:
         return self.email
