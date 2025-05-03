28d3cf596fb39cb41abd1fa4c3ce521ddcbfba62
RichardMatheus929 <richard.ataliba.711@ufrn.edu.br>
2025-05-03 18:44:53 -0300

    feat: return who likes a post

--- twitter/posts/serializers.py
+++ twitter/posts/serializers.py
@@ -9,6 +9,11 @@ class PostSerializer(serializers.ModelSerializer):
 
     created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
 
+    likes_user = serializers.SerializerMethodField()
+
+    def get_likes_user(self, obj):
+        return obj.likes.all().values_list("user__id", flat=True)
+
     class Meta:
         model = Post
-        fields = ['id', 'user', 'content', 'created_at']
+        fields = ['id', 'user', 'content', 'created_at', 'likes_user']
